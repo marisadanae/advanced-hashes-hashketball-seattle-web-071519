ac00879b1aaaeb733550e92afec1f133701f422c
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-13 17:25:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,19 +17,19 @@ def game_hash
  :colors => ["Black", "White"],
  :players => [
 
-    "Andy Anders" => { player_name: "Andy Anderson", number:  0, shoe: 16, points: 22,rebounds: 12, assists: 12, steals: 3,
+    { player_name: "Andy Anderson", number:  0, shoe: 16, points: 22,rebounds: 12, assists: 12, steals: 3,
                   blocks: 1, slam_dunks: 1},
 
-    "Reggie Evans" =>  { player_name: "Reggie Evans", number: 30, shoe: 14, points: 12,rebounds: 12, assists: 12, steals: 12,
+    { player_name: "Reggie Evans", number: 30, shoe: 14, points: 12,rebounds: 12, assists: 12, steals: 12,
                   blocks: 12, slam_dunks: 7},
 
-    "Brook Lopez" => { player_name: "Brook Lopez", number: 11, shoe: 17, points: 7,rebounds: 19, assists: 10, steals: 3,
+   { player_name: "Brook Lopez", number: 11, shoe: 17, points: 7,rebounds: 19, assists: 10, steals: 3,
                   blocks: 1, slam_dunks: 5},
 
-    "Mason Plumlee" => { player_name: "Mason Plumlee", number: 1, shoe: 19, points: 26,rebounds: 11, assists: 16, steals: 3,
+    { player_name: "Mason Plumlee", number: 1, shoe: 19, points: 26,rebounds: 11, assists: 16, steals: 3,
                   blocks: 8, slam_dunks: 5},
 
-    "Jason Terry" => { player_name: "Jason Terry", number: 31, shoe: 15, points: 19,rebounds: 2, assists: 2, steals: 4,
+    { player_name: "Jason Terry", number: 31, shoe: 15, points: 19,rebounds: 2, assists: 2, steals: 4,
                   blocks: 11, slam_dunks: 1}
 
     ]
@@ -40,19 +40,19 @@ def game_hash
   :colors => ["Turquoise", "Purple"],
 
     :players => [
-  "Jeff Adrien" => { player_name: "Jeff Adrien", number: 4, shoe: 18, points: 10,rebounds: 1, assists: 1, steals: 2,
+  { player_name: "Jeff Adrien", number: 4, shoe: 18, points: 10,rebounds: 1, assists: 1, steals: 2,
           blocks: 7, slam_dunks: 2},
 
-    "Bismack Biyombo" => { player_name: "Bismack Biyombo", number: 0, shoe: 16, points: 12,rebounds: 4, assists: 7, steals: 22,
+    { player_name: "Bismack Biyombo", number: 0, shoe: 16, points: 12,rebounds: 4, assists: 7, steals: 22,
           blocks: 15, slam_dunks: 10},
 
-    "DeSagna Diop" => { player_name: "DeSagna Diop", number: 2, shoe: 14, points: 24,rebounds: 12, assists: 12, steals: 4,
+    { player_name: "DeSagna Diop", number: 2, shoe: 14, points: 24,rebounds: 12, assists: 12, steals: 4,
           blocks: 5, slam_dunks: 5},
 
-    "Ben Gordan" => { player_name: "Ben Gordan", number: 8, shoe: 15, points: 33,rebounds: 3, assists: 2, steals: 1,
+    { player_name: "Ben Gordan", number: 8, shoe: 15, points: 33,rebounds: 3, assists: 2, steals: 1,
           blocks: 1, slam_dunks: 0},
 
-    "Kemba Walker" => { player_name: "Kemba Walker", number: 33, shoe: 15, points: 6,rebounds: 12, assists: 12, steals: 7,
+    { player_name: "Kemba Walker", number: 33, shoe: 15, points: 6,rebounds: 12, assists: 12, steals: 7,
           blocks: 5, slam_dunks: 12},
         ]
 
