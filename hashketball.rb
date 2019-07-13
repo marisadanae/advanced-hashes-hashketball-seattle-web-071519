3e3e36bbb0a987c91cd2421c653804fc5206139e
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-13 16:13:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -23,7 +23,7 @@ def game_hash
     "Jason Terry" => { player_name: "Jason Terry", number: 31, shoe: 15, points: 19,rebounds: 2, assists: 2, steals: 4,
                   blocks: 11, slam_dunks: 1}
 
-    ] }
+    ] 
     },
 
   :away => {
